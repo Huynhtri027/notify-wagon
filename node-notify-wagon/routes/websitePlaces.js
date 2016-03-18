136caa97722b137e504845367fcd0f13e8539b28
stevensolivier <stevensolivier@connecthings.com>
2016-03-18 14:31:20 +0100

    #Node: few backend ui done

--- node-notify-wagon/routes/websitePlaces.js
+++ node-notify-wagon/routes/websitePlaces.js
@@ -8,7 +8,7 @@ module.exports = [
     handler: function (request, reply) {
       var answer = servicePlaces.find(request.params.type, request.params.id);
       console.log(answer);
-      reply.view('detail', {root:answer, avatar:request.params.type === "stations"?"home":"train"});
+      reply.view('detail', {root:answer, avatar:'train'});
     }
   },
   {
